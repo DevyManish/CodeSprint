2a68688125f4f14895b3a437b01214a23d27b42a
Manish Gupta <121728309+DevyManish@users.noreply.github.com>
2024-08-21 23:54:33 +0530

    init commit

---
+++ src/components/SplineDesign.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+const SplineDesign = () => {
+  return (
+    <Spline scene="https://prod.spline.design/vp8b-Cvsytj8jJOV/scene.splinecode" />
+  )
+}
+
+export default SplineDesign
+
